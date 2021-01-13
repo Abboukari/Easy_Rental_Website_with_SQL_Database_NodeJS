8b35fa9040470203b63caec001d7300fed424f9b
Abboukari <71227631+Abboukari@users.noreply.github.com>
2021-01-13 10:20:37 +0100

    Error handling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
+const createError = require('http-errors')
 
 const Fietsendisplay = require('./services/fietsendisplay');
 const MedewerkerService = require('./services/MedewerkerService');
@@ -12,7 +13,9 @@ const medewerkerService = new MedewerkerService('./data/medewerkers.json');
 const klantdisplay = new Klantdisplay('./data/klant.json');
 
 const routes = require('./routes');
-
+const { request } = require('http');
+const { response } = require('express');
+// const { request, response } = require('express');
 // const { Session } = require('inspector');
 
 const app = express();
@@ -39,6 +42,20 @@ app.use(
     klantdisplay,
   })
 );
+
+app.use((request, response, next) => {
+  return next(createError(404,'File not found'));
+});
+
+app.use((err, request, response, next) => {
+  response.locals.message = err.message;
+  console.error(err);
+  const status = err.status || 500;
+  response.locals.status = status;
+  response.status(status);
+  response.render('error');
+});
+
 
 app.listen(port, () => {
   console.log('This port is up and running');
